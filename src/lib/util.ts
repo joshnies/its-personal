91508f40ac95a75f606008190e15108821f85af8
Josh Nies <50473759+joshnies@users.noreply.github.com>
2025-06-03 07:36:21 -0400

    chore: rm console log

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,4 @@
 export function isMobile() {
-  console.log("userAgent:", navigator.userAgent);
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent,
   );
